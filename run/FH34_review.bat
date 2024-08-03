c6653bfd21debc61cb87ac720e1160aa286bb26a
Andrew <andyfaichney2803@gmail.com>
2024-08-03 12:59:58 +0100

    enter commit

---
+++ run/FH34_review.bat
@@ -0,0 +1,2 @@
+python solve_regular.py --USE_FH 34 --datasource review35
+python solve_regular.py --USE_FH 34 --datasource review36
---
+++ run/FH37_review.bat
@@ -0,0 +1,2 @@
+python solve_regular.py --USE_FH 37 --datasource review35
+python solve_regular.py --USE_FH 37 --datasource review36
